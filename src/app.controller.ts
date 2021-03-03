88bca80985bf243613e02de008c1d3ac34f56b0b
pricelessnothing <edaniile@mail.ru>
2021-03-03 15:50:58 +0300

    swagger implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './user/user.entity';
 import { UserService } from './user/user.service';
 
 @Controller()
+@ApiTags('users')
 export class AppController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): Promise<User[]> {
+  @ApiResponse({
+    status: 200,
+    description: 'List of users',
+    type: [User],
+  })
+  getAllUsers(): Promise<User[]> {
     return this.userService.findAll();
   }
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   email: string;
 }
